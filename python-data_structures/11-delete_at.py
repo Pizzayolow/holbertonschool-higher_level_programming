c41e2d3a7005ac5e58492b0fadec62e51050cf79
Pizzayolow <ralliard.valentin@gmail.com>
2023-06-06 11:42:55 +0200

    pyc friendly

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -4,9 +4,9 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
     for i in my_list:
-        if i != idx:
+        if i != idx + 1:
             new_list_with_del.append(i)
     my_list.clear()
     for i in new_list_with_del:
-            my_list.append(i)
+        my_list.append(i)
     return my_list
