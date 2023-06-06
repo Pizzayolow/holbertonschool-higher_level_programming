bec8da895c1d5251230d77d2f710e601a4cb6087
Pizzayolow <ralliard.valentin@gmail.com>
2023-06-06 10:55:28 +0200

    try to handle with the checkker 10

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,6 +2,8 @@
 def max_integer(my_list=[]):
     if my_list is None:
         return
+    if my_list.count == 0:
+        return
     maxvalue = my_list[0]
     for i in my_list:
         if maxvalue < i:
