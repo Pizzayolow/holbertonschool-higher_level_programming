3c1ddd3d618efcf34b03a884136bcabb882468b4
Pizzayolow <ralliard.valentin@gmail.com>
2023-06-06 10:24:40 +0200

    try to pass a checker

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
-        return
+    if len(sentence) == 0:
+        return len(sentence), None
     return(len(sentence), sentence[0])
