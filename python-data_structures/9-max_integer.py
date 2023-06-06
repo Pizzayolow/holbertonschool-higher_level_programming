bf3e050f91543b0da42597e41baa24741601988e
Pizzayolow <ralliard.valentin@gmail.com>
2023-06-06 10:56:28 +0200

    second time

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None:
-        return
     if my_list.count == 0:
+        return
+    if my_list is None:
         return
     maxvalue = my_list[0]
     for i in my_list:
