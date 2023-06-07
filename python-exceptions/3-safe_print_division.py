30deac49a3f53b5f6537b0567874b4cc8bf29a19
Pizzayolow <ralliard.valentin@gmail.com>
2023-06-07 15:44:38 +0200

    ok

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,8 +3,8 @@ def safe_print_division(a, b):
     sum = 0
     try:
         sum = a / b
+        return sum
     except ZeroDivisionError:
-        return
+        sum = None
     finally:
         print("Inside result: {}".format(sum))
-        return sum
