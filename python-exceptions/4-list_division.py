787369e6187aeb30b96592e5509a669ff9996d4e
Pizzayolow <ralliard.valentin@gmail.com>
2023-06-08 10:51:48 +0200

    try to pass all the checkers

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -6,6 +6,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
         except TypeError:
+            result = 0
             print("wrong type")
         except ZeroDivisionError:
             result = 0
