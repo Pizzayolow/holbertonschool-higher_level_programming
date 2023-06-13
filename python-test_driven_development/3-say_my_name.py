c97ff9e7f8532bf95e2965342ae7bf5d38c79cb9
Pizzayolow <ralliard.valentin@gmail.com>
2023-06-13 15:40:59 +0200

    add comments in the function part

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -6,6 +6,9 @@ return name and lastname
 
 
 def say_my_name(first_name, last_name=""):
+    """
+    check if the variables passed are string and print the names
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
