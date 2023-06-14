0c7956dfc128a5e10ec0295d8c922e1a06971850
Pizzayolow <ralliard.valentin@gmail.com>
2023-06-14 15:37:44 +0200

    add classes

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+print a rectangle
+"""
+
+
+class Rectangle:
+    """rectangle"""
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self, value):
+        return self._Rectangle__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self._Rectangle__width = value
+
+    @property
+    def height(self, value):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self._Rectangle__height = value
