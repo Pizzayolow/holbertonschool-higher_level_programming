21aa3d20e8f7acb1f139d132c8eb044b9c4062cd
Pizzayolow <ralliard.valentin@gmail.com>
2023-06-14 16:15:57 +0200

    add area and perimeter

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+print a rectangle
+"""
+
+
+class Rectangle:
+    """rectangle"""
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self._Rectangle__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self._Rectangle__width = value
+
+    @property
+    def height(self):
+        return self._Rectangle__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self._Rectangle__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        perimeter = 0
+        if self.width or self.height == 0:
+            perimeter = 0
+        perimeter = (self.width + self.height) * 2
+        return perimeter
