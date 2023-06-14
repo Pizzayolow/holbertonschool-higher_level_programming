07e939abf801b3f74ce28e7b8cac20ecfdbb2164
Pizzayolow <ralliard.valentin@gmail.com>
2023-06-14 11:23:57 +0200

    fixed

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,52 +1,49 @@
 #!/usr/bin/python3
-"""UNITTEST OF MAX INTEGER
+"""Unittest for max_integer([..])
 """
-
-
 import unittest
-max_integer = __import__('6-max_integer').max_integer
+
+max_integer = __import__("6-max_integer").max_integer
+
 
 class TestMaxInteger(unittest.TestCase):
     def test_greater(self):
-        list = [10, 22, 11, 5, 1, 4, 7]
-        self.assertEqual(max_integer(list), 22)
-        
-    def test_unique(self):
-        list = [22]
-        self.assertEqual(max_integer(list), 22)
-        
+        list = [5, 1, 10, 3]
+        self.assertEqual(max_integer(list), 10)
+
+    def test_uniq(self):
+        list = [10]
+        self.assertEqual(max_integer(list), 10)
+
     def test_dual(self):
-        list = [10, 22]
-        self.assertEqual(max_integer(list), 22)
-        
+        list = [10, 5]
+        self.assertEqual(max_integer(list), 10)
+
     def test_mirror(self):
-        list = [22, 22]
-        self.assertEqual(max_integer(list), 22)
-        
+        list = [10, 10]
+        self.assertEqual(max_integer(list), 10)
+
     def test_empty(self):
         list = []
         self.assertIsNone(max_integer(list))
-        
+
     def test_str(self):
-        list = ["Simon"]
-        self.assertEqual(max_integer(list), "Simon")
-        
-    def test_neg(self):
-        list = [-10, 22, 11, 5, -100, 4, 7]
-        self.assertEqual(max_integer(list), 22)
-        
-    def test_floats(self):
-        list = [10, 22.88, 11.5, 5, 1, 4, 7]
-        self.assertEqual(max_integer(list), 22.88)
+        list = ["simon"]
+        self.assertEqual(max_integer(list), "simon")
+
+    def test_negative(self):
+        list = [-1, -12, -10, -100, 6]
+        self.assertEqual(max_integer(list), 6)
+
+    def test_float(self):
+        list = [3.667, 3.668]
+        self.assertEqual(max_integer(list), 3.668)
 
     def test_is_list(self):
-        data = [10, 22, 11, 5, 1, 4, 7]
+        data = [3.667, 3.668]
         self.assertIsInstance(data, list)
-        
-    def test_is_int_or_float(self):
-        data = [10, 22.88, 11, 88, 1, 4, 7]
-        for element in range(len(data)):
-            self.assertIsInstance(data[element], (int, float))
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_is_int_or_float(self):
+        data = [3, 3.668]
+        for i in range(len(data)):
+            self.assertIsInstance(data[i], (int, float))
