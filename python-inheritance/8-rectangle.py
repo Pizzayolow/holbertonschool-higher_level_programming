0f1a696e3fa1406c21d24ce7bca16261df51ff2b
Pizzayolow <ralliard.valentin@gmail.com>
2023-06-20 14:34:01 +0200

    <sdfgq<dsfgh

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,6 +4,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    '''rectangle'''
     def __init__(self, width, height):
         self.__width = BaseGeometry.integer_validator(self, '', width)
         self.__height = BaseGeometry.integer_validator(self, '', height)
