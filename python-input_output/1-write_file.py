8aff8a798ddf7f81e0de2cc3766e6b3d767a4ca6
Pizzayolow <ralliard.valentin@gmail.com>
2023-06-21 11:19:16 +0200

    qsgq

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,7 +4,5 @@
 
 def write_file(filename="", text=""):
     '''append to a file'''
-    if filename == None:
-        f = open(filename, "x", encoding='utf-8')
     with open(filename, "w", encoding='utf-8') as f:
         return f.write(text)
