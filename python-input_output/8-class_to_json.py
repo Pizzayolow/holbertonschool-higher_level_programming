e04c9064ff9a9cedb8f726263fd70d05ae2134fc
Pizzayolow <ralliard.valentin@gmail.com>
2023-06-21 16:07:21 +0200

    qsdfrghqdfh

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,9 +2,19 @@
 '''class to JSON'''
 
 
-import json
-
-
 def class_to_json(obj):
-    '''class to JSON'''
-    return json.dumps(obj.__dict__)
+    # Check if the object is a dictionary, list, string, integer, or boolean
+    if isinstance(obj, dict):
+        return {key: class_to_json(value) for key, value in obj.items()}
+    elif isinstance(obj, list):
+        return [class_to_json(item) for item in obj]
+    elif isinstance(obj, str) or isinstance(obj, int) or isinstance(obj, bool):
+        return obj
+    
+    # If the object is not a basic data type, convert it to a dictionary
+    attributes = {}
+    for attr in obj.__dict__:
+        value = getattr(obj, attr)
+        if isinstance(value, (list, dict, str, int, bool)):
+            attributes[attr] = class_to_json(value)
+    return attributes
