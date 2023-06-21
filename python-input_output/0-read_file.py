829d63f2eddb9b2f381db73ec4ef15c193c3f5ed
Pizzayolow <ralliard.valentin@gmail.com>
2023-06-21 11:00:26 +0200

    sdf

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,5 @@
 def read_file(filename=""):
     '''read file and close'''
     with open(filename, encoding='utf-8') as f:
-        print(f.read())
+        for line in f:
+            print(line, end='')
