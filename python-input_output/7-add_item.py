7dfab756fb55454900881294b057de06c332521f
Pizzayolow <ralliard.valentin@gmail.com>
2023-06-21 14:52:12 +0200

    qsrgqs

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-
-
+"""write argv in a file"""
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import os
 
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+args = sys.argv
 filename = "add_item.json"
-my_list = []
-for args in range(1, len(sys.argv)):
-    my_list.append(sys.argv[args])
-print(my_list)
+
+if os.path.isfile(filename):
+    my_list = load_from_json_file(filename)
+else:
+    my_list = []
+
+my_list.extend(args[1:])
+
 save_to_json_file(my_list, filename)
