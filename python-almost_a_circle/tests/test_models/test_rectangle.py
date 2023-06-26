496f2a12aeeed6291ef37973f08190e073958611
Pizzayolow <ralliard.valentin@gmail.com>
2023-06-26 15:58:07 +0200

    add elses

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,17 +1,15 @@
 import unittest
-
 from models.rectangle import Rectangle
-
 
 class TestRectangle(unittest.TestCase):
     def setUp(self):
         Rectangle.id = 0
         self.rect1 = Rectangle(5, 10, 2, 3)
         self.rect2 = Rectangle(5, 10, 2, 3, 100)
+        
 
     def test_aid(self):
-        # Assuming Base class does not generate ids
-        self.assertEqual(self.rect1.id, 1)
+        self.assertEqual(self.rect1.id, 1)  
         self.assertEqual(self.rect2.id, 100)
 
     def test_dimensions(self):
@@ -31,6 +29,26 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(self.rect1.x, 4)
         self.assertEqual(self.rect1.y, 6)
 
+    def test_invalid_values(self):
+        with self.assertRaises(TypeError):
+            self.rect1.width = "20"
+        with self.assertRaises(ValueError):
+            self.rect1.width = -10
+        with self.assertRaises(TypeError):
+            self.rect1.height = "30"
+        with self.assertRaises(ValueError):
+            self.rect1.height = 0
+        with self.assertRaises(TypeError):
+            self.rect1.x = "4"
+        with self.assertRaises(ValueError):
+            self.rect1.x = -4
+        with self.assertRaises(TypeError):
+            self.rect1.y = "6"
+        with self.assertRaises(ValueError):
+            self.rect1.y = -6
+
+    def tearDown(self):
+        Rectangle.id = 0
 
 if __name__ == "__main__":
     unittest.main()
