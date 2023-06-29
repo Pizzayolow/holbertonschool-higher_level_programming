bad6114c035ef37c6504f7dcc2e2fefc605fdcb8
Pizzayolow <ralliard.valentin@gmail.com>
2023-06-29 10:57:14 +0200

    final tests

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -60,7 +60,7 @@ class Square(Rectangle):
 
     def to_dictionary(self):
         '''dict representation of square'''
-        return{
+        return {
             'id': self.id,
             'x': self.x,
             'size': self.size,
