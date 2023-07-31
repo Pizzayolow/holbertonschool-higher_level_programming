c553c0edde15de437bb2da40128ad3ade1f06ca4
Pizzayolow <ralliard.valentin@gmail.com>
2023-07-31 15:09:27 +0200

    arizona

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -10,11 +10,11 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],
+        db=sys.argv[3]
         )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE \
-        BINARY 'N%' ORDER BY states.id")
+    cur.execute("SELECT * FROM states WHERE BINARY name='{}' \
+        ORDER BY states.id".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
