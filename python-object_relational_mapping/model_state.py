63480bb839da407218aa2f8a1a5dddf9dcabb58e
Pizzayolow <ralliard.valentin@gmail.com>
2023-08-01 11:00:15 +0200

    task7

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Base class"""
-from sqlalchemy import Column, Integer, String, Table
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
