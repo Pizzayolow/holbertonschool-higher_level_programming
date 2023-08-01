c112ec46db928034adc713fba1b7b59a3462645a
Pizzayolow <ralliard.valentin@gmail.com>
2023-08-01 11:29:18 +0200

    task 9

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""A script that lists all State objects from the database hbtn_0e_6_usa"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import Base, State
+
+if __name__ == "__main__":
+    """Create an engine"""
+    engine = create_engine("mysql://{}:{}@localhost:3306/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    """Create a session"""
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    """Query the database and format output"""
+    s = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
+    if s is None:
+        print("Nothing")
+
+    else:
+        for row in s:
+            print("{}: {}".format(row.id, row.name))
+
+    """Close the session"""
+    session.close()
